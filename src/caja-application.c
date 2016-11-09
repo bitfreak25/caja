3564314fe742151a223a85d5036f77edd6a4c089
bitfreak25 <bitfreak25@gmx.de>
2016-11-09 21:21:08 +0100

    Initialize Pointer *geometry with NULL
    Set Pointer *geometry on initialize to NULL, cause when the if-Statement will not be true, the function open_windows() will get a uninitialized variable.

--- src/caja-application.c
+++ src/caja-application.c
@@ -451,7 +451,7 @@ caja_application_open (GApplication *app,
 {
     CajaApplication *self = CAJA_APPLICATION (app);
     gboolean browser_window = FALSE;
-    const gchar *geometry;
+    const gchar *geometry = NULL;
     const char splitter = '=';
 
     g_debug ("Open called on the GApplication instance; %d files", n_files);
